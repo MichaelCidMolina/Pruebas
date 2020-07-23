62e756a987ccff7f26d1be836a05de7ff0a89140
Michael Cid Molina <johnnymike@kali.Bicho.com>
2020-07-22 17:39:54 -0700

    Commit Prueba

---
+++ Castlevanias.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd 
+mkdir Castlevanias
+cd Castlevanias
+
+#Emulador
+curl -O https://romsmania.cc/emulators/gameboy-advance/visualboyadvance-68
+#roms Castlevania
+
+#Aria of sorrow
+curl -o CAOS.zip  https://romsmania.cc/roms/gameboy-advance/castlevania-aria-of-sorrow-eurasia-223660
+
+#Circle of the Moon
+curl -o COTM.zip  https://romsmania.cc/roms/gameboy-advance/castlevania-circle-of-the-moon-eurasia-223662
+
+#Harmony of Dissonance
+curl -o HOD.zip  https://romsmania.cc/roms/gameboy-advance/castlevania-harmony-of-dissonance-eurasia-223664
+
+
